46ca375f3d3cb4d2804778d7df0cac721da32250
GibsonCS <gibson.cruzds@gmail.com>
2026-03-19 23:36:19 -0300

    feat(email): prevent invalid email creation

--- src/main/java/br/com/codelift/speed/domain/vo/Email.java
+++ src/main/java/br/com/codelift/speed/domain/vo/Email.java
@@ -1,14 +1,25 @@
 package br.com.codelift.speed.domain.vo;
 
+import br.com.codelift.speed.exception.BusinessException;
+
 import java.util.Objects;
 
 public class Email {
 
-    private String value;
+    private final String value;
 
     public static Email create(String value) {
+        validate(value);
 
         return new Email(value);
+    }
+
+    private static void validate(String email) {
+        if (email == null || email.isBlank()) throw new BusinessException("Email cannot be empty");
+
+        String regex = "^[a-zA-Z0-9]+([._-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9-]+(\\.[a-zA-Z]{2,})+$";
+
+        if (!email.matches(regex)) throw new BusinessException("Invalid email");
     }
 
     private Email(String value) {
