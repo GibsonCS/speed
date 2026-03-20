5d5a63565d06664e0a9619418c387dc0871d45a8
GibsonCS <gibson.cruzds@gmail.com>
2026-03-19 22:51:56 -0300

    feat(name): implement name creation

---
+++ src/main/java/br/com/codelift/speed/domain/vo/Name.java
@@ -0,0 +1,44 @@
+package br.com.codelift.speed.domain.vo;
+
+import br.com.codelift.speed.exception.BusinessException;
+
+import java.util.Objects;
+
+public class Name {
+    private final String value;
+
+    private Name(String value) {
+        this.value = value;
+    }
+
+    public static Name create(String value) {
+//        validate(value);
+        return new Name(value);
+    }
+
+    private static void validate(String name) {
+        if (name == null || name.isBlank()) throw new BusinessException("Name cannot be null");
+        if (name.length() < 3) throw new BusinessException("Name cannot be less than 3 characters");
+        if (name.length() > 20) throw new BusinessException("Name cannot be more than 20 characters");
+
+        if (!name.matches("^[a-zA-ZÀ-ÿ ]+$")) {
+            throw new BusinessException("Name cannot contain special characters");
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Name name)) return false;
+        return Objects.equals(value, name.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
