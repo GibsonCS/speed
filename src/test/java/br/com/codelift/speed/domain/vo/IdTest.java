22b6109aa6ee536adf3c169479beec58b211bd5e
GibsonCS <gibson.cruzds@gmail.com>
2026-03-20 20:55:46 -0300

    test(id): should not allow null id creation

--- src/test/java/br/com/codelift/speed/domain/vo/IdTest.java
+++ src/test/java/br/com/codelift/speed/domain/vo/IdTest.java
@@ -1,10 +1,12 @@
 package br.com.codelift.speed.domain.vo;
 
+import br.com.codelift.speed.exception.BusinessException;
 import org.junit.jupiter.api.Test;
 
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class IdTest {
 
@@ -14,8 +16,12 @@ class IdTest {
     void shouldReturnAValidId() {
 
         Id id = Id.create(VALID_ID);
-
+        
         assertEquals(VALID_ID, id.getValue());
     }
 
+    @Test
+    void shouldNotCreateWithNullId() {
+        assertThrows(BusinessException.class, () -> Id.create(null));
+    }
 }
