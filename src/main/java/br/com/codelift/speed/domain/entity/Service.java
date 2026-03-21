f5ee6e0aa3ebc65ac8e3e5e2f1add1bfd059c069
GibsonCS <gibson.cruzds@gmail.com>
2026-03-20 21:02:56 -0300

    refactor(service): improve readability

--- src/main/java/br/com/codelift/speed/domain/entity/Service.java
+++ src/main/java/br/com/codelift/speed/domain/entity/Service.java
@@ -14,7 +14,56 @@ public class Service {
     private BigDecimal price;
     private final ServiceStatus status;
 
-    private Service(UUID id, String name, String description, BigDecimal price, ServiceStatus status) {
+    public static Service create(
+            UUID id,
+            String name,
+            String description,
+            BigDecimal price,
+            ServiceStatus status
+    ) {
+        validateName(name);
+        validatePrice(price);
+        validateDescription(description);
+
+        return new Service(id, name, description, price, status);
+    }
+
+    private static void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new BusinessException("Name cannot be null");
+        }
+    }
+
+    private static void validatePrice(BigDecimal price) {
+        if (price.compareTo(BigDecimal.valueOf(50)) < 0) {
+            throw new BusinessException("Value cannot be less than R$50,00");
+        }
+    }
+
+    private static void validateDescription(String description) {
+        if (description == null || description.isBlank()) {
+            throw new BusinessException("Description cannot be null");
+        }
+    }
+
+    public void disableService() {
+        if (this.status == ServiceStatus.DISABLE) {
+            throw new BusinessException("Service is already disable");
+        }
+    }
+
+    public void updatePrice(BigDecimal newPrice) {
+        validatePrice(price);
+        this.price = newPrice;
+    }
+
+    private Service(
+            UUID id,
+            String name,
+            String description,
+            BigDecimal price,
+            ServiceStatus status
+    ) {
         this.id = id;
         this.name = name;
         this.description = description;
@@ -40,46 +89,5 @@ public class Service {
 
     public ServiceStatus getActive() {
         return status;
-    }
-
-    public static Service create(
-            UUID id,
-            String name,
-            String description,
-            BigDecimal price,
-            ServiceStatus status
-    ) {
-        validateName(name);
-        validatePrice(price);
-        validateDescription(description);
-
-        return new Service(id, name, description, price, status);
-    }
-
-    private static void validateName(String name) {
-        if (name == null || name.isBlank()) throw new BusinessException("Name cannot be null");
-    }
-
-    private static void validatePrice(BigDecimal price) {
-        if (price.compareTo(BigDecimal.valueOf(50)) < 0) {
-            throw new BusinessException("Value cannot be less than R$50,00");
-        }
-    }
-
-    private static void validateDescription(String description) {
-        if (description == null || description.isBlank()) {
-            throw new BusinessException("Description cannot be null");
-        }
-    }
-
-    public void disableService() {
-        if (this.status == ServiceStatus.DISABLE) {
-            throw new BusinessException("Service is already disable");
-        }
-    }
-
-    public void updatePrice(BigDecimal newPrice) {
-        validatePrice(price);
-        this.price = newPrice;
     }
 }
