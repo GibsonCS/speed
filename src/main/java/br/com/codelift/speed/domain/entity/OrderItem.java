7d4e9116e6db24be12a29e8230e79acfbbeb0f25
GibsonCS <gibson.cruzds@gmail.com>
2026-03-20 21:11:45 -0300

    refactor(order-item): improve readability

--- src/main/java/br/com/codelift/speed/domain/entity/OrderItem.java
+++ src/main/java/br/com/codelift/speed/domain/entity/OrderItem.java
@@ -15,6 +15,44 @@ public class OrderItem {
     private final OrderItemStatus status;
     private final LocalDate executionDate;
     private final BigDecimal chargedPrice;
+    
+    public static OrderItem create(
+            UUID serviceId,
+            Address executionAddress,
+            OrderItemStatus status,
+            LocalDate executionDate,
+            BigDecimal chargedPrice
+    ) {
+        validateServiceId(serviceId);
+        validateExecutionDate(executionDate);
+        validateChargedPrice(chargedPrice);
+
+        return new OrderItem(
+                serviceId,
+                executionAddress,
+                status,
+                executionDate,
+                chargedPrice
+        );
+    }
+
+    private static void validateServiceId(UUID serviceId) {
+        if (serviceId == null) {
+            throw new BusinessException("Service id cannot be null");
+        }
+    }
+
+    private static void validateExecutionDate(LocalDate executionDate) {
+        if (executionDate.isBefore(LocalDate.now())) {
+            throw new BusinessException("Invalid date");
+        }
+    }
+
+    private static void validateChargedPrice(BigDecimal chargedPrice) {
+        if (chargedPrice.compareTo(BigDecimal.valueOf(0)) <= 0) {
+            throw new BusinessException("Charged price must be more than 0");
+        }
+    }
 
     private OrderItem(
             UUID serviceId,
@@ -48,38 +86,5 @@ public class OrderItem {
 
     public BigDecimal getChargedPrice() {
         return chargedPrice;
-    }
-
-    public static OrderItem create(
-            UUID serviceId,
-            Address executionAddress,
-            OrderItemStatus status,
-            LocalDate executionDate,
-            BigDecimal chargedPrice
-    ) {
-        validateServiceId(serviceId);
-        validateExecutionDate(executionDate);
-        validateChargedPrice(chargedPrice);
-
-        return new OrderItem(
-                serviceId,
-                executionAddress,
-                status, executionDate,
-                chargedPrice
-        );
-    }
-
-    private static void validateServiceId(UUID serviceId) {
-        if (serviceId == null) throw new BusinessException("Service id cannot be null");
-    }
-
-    private static void validateExecutionDate(LocalDate executionDate) {
-        if (executionDate.isBefore(LocalDate.now())) throw new BusinessException("Invalid date");
-    }
-
-    private static void validateChargedPrice(BigDecimal chargedPrice) {
-        if (chargedPrice.compareTo(BigDecimal.valueOf(0)) <= 0) {
-            throw new BusinessException("Charged price must be more than 0");
-        }
     }
 }
