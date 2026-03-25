3237a95f1ab81ee891f54718aaa9d204dc46ccd3
GibsonCS <gibson.cruzds@gmail.com>
2026-03-25 20:34:38 -0300

    feat(order-status): remove submitted

--- src/main/java/br/com/codelift/speed/domain/entity/enums/OrderStatus.java
+++ src/main/java/br/com/codelift/speed/domain/entity/enums/OrderStatus.java
@@ -1,5 +1,5 @@
 package br.com.codelift.speed.domain.entity.enums;
 
 public enum OrderStatus {
-    DRAFT, SUBMITTED, IN_ANALISE, REJECTED, CONFIRMED, PAID, CANCELED
+    DRAFT, IN_ANALISE, REJECTED, CONFIRMED, PAID, CANCELED
 }
