e5fe406d824265b975dbbd522025b5b23ace4f49
GibsonCS <gibson.cruzds@gmail.com>
2026-03-25 20:09:54 -0300

    feat(order-status): add news status

--- src/main/java/br/com/codelift/speed/domain/entity/enums/OrderStatus.java
+++ src/main/java/br/com/codelift/speed/domain/entity/enums/OrderStatus.java
@@ -1,5 +1,5 @@
 package br.com.codelift.speed.domain.entity.enums;
 
 public enum OrderStatus {
-    PENDING, PAID, CANCELED, CONFIRMED
+    DRAFT, SUBMITTED, IN_ANALISE, REJECTED, CONFIRMED, PAID, CANCELED
 }
