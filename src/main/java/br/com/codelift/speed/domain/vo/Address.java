f43de83fde923913bcbb66808d64c61af2ae4a9c
GibsonCS <gibson.cruzds@gmail.com>
2026-03-27 21:58:15 -0300

    refactor(address): improve readability

--- src/main/java/br/com/codelift/speed/domain/vo/Address.java
+++ src/main/java/br/com/codelift/speed/domain/vo/Address.java
@@ -9,12 +9,21 @@ public class Address {
     private final String uf;
     private final String zipCode;
 
-    public static Address create(String street, String neighborhood, String uf, String zipCode) {
-
+    public static Address create(
+            String street,
+            String neighborhood,
+            String uf,
+            String zipCode
+    ) {
         return new Address(street, neighborhood, uf, zipCode);
     }
 
-    private Address(String street, String neighborhood, String uf, String zipCode) {
+    private Address
+            (String street,
+             String neighborhood,
+             String uf,
+             String zipCode
+            ) {
         this.street = street;
         this.neighborhood = neighborhood;
         this.uf = uf;
@@ -43,7 +52,8 @@ public class Address {
 
         if (!(o instanceof Address address)) return false;
 
-        return Objects.equals(street, address.street) && Objects.equals(neighborhood, address.neighborhood)
+        return Objects.equals(street, address.street)
+                && Objects.equals(neighborhood, address.neighborhood)
                 && Objects.equals(uf, address.uf)
                 && Objects.equals(zipCode, address.zipCode
         );
