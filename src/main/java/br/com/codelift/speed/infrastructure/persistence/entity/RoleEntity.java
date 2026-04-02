0ee2fd349b40a807caa13f92a43e0a2799a684b5
GibsonCS <gibson.cruzds@gmail.com>
2026-04-02 20:01:01 -0300

    feat(infra): remove lombok from role

--- src/main/java/br/com/codelift/speed/infrastructure/persistence/entity/RoleEntity.java
+++ src/main/java/br/com/codelift/speed/infrastructure/persistence/entity/RoleEntity.java
@@ -3,13 +3,12 @@ package br.com.codelift.speed.infrastructure.persistence.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.Data;
 
 import java.util.UUID;
 
 @Entity
 @Table(name = "roles")
-@Data
+
 public class RoleEntity {
 
     @Id
@@ -22,5 +21,21 @@ public class RoleEntity {
     public RoleEntity(String name, UUID id) {
         this.name = name;
         this.id = id;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 }
