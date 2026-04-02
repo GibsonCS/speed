aa952f297866fbcf3c01537193e7d58a3cbba988
GibsonCS <gibson.cruzds@gmail.com>
2026-04-02 20:01:42 -0300

    feat(infra): add find by name method

--- src/main/java/br/com/codelift/speed/infrastructure/persistence/repository/JpaRoleRepository.java
+++ src/main/java/br/com/codelift/speed/infrastructure/persistence/repository/JpaRoleRepository.java
@@ -3,7 +3,9 @@ package br.com.codelift.speed.infrastructure.persistence.repository;
 import br.com.codelift.speed.infrastructure.persistence.entity.RoleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface JpaRoleRepository extends JpaRepository<RoleEntity, UUID> {
+    Optional<RoleEntity> findByName(String name);
 }
