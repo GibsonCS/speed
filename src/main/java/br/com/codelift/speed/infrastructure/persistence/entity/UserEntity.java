ee5186b02054ebbbbdd515eb78f7fb12e7c5d3cb
GibsonCS <gibson.cruzds@gmail.com>
2026-04-02 20:00:19 -0300

    refactor(infra): user entity

--- src/main/java/br/com/codelift/speed/infrastructure/persistence/entity/UserEntity.java
+++ src/main/java/br/com/codelift/speed/infrastructure/persistence/entity/UserEntity.java
@@ -1,17 +1,13 @@
 package br.com.codelift.speed.infrastructure.persistence.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
-import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.*;
 
+import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
 
 @Entity
 @Table(name = "users")
-@Data
 public class UserEntity {
 
     @Id
@@ -22,5 +18,58 @@ public class UserEntity {
     private String password;
 
     @ManyToMany
-    private Set<RoleEntity> roles;
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<RoleEntity> roles = new HashSet<>();
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<RoleEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleEntity> roles) {
+        this.roles = roles;
+    }
 }
