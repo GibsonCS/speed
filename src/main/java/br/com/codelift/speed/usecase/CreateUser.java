6c11d488cccaca4201a2b0d3ed9d0be0e3b43481
GibsonCS <gibson.cruzds@gmail.com>
2026-04-02 13:17:30 -0300

    feat(use-case): prevent duplicated email

--- src/main/java/br/com/codelift/speed/usecase/CreateUser.java
+++ src/main/java/br/com/codelift/speed/usecase/CreateUser.java
@@ -1,25 +1,40 @@
 package br.com.codelift.speed.usecase;
 
 import br.com.codelift.speed.domain.entity.User;
-import br.com.codelift.speed.domain.vo.Email;
-import br.com.codelift.speed.domain.vo.Name;
-import br.com.codelift.speed.usecase.infrastructure.web.dto.UserRequest;
-import br.com.codelift.speed.usecase.infrastructure.web.dto.UserResponse;
+import br.com.codelift.speed.domain.repository.UserRepository;
+import br.com.codelift.speed.exception.BusinessException;
+import br.com.codelift.speed.infrastructure.web.dto.UserRequest;
+import br.com.codelift.speed.infrastructure.web.dto.UserResponse;
 
 import java.util.UUID;
 
 public class CreateUser {
 
+    UserRepository userRepository;
+
+    public CreateUser(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public UserResponse execute(UserRequest userRequest) {
 
+        if (userRepository.findByEmail(userRequest.email()).isPresent()) {
+            throw new BusinessException("User not exists");
+        }
+
         UUID id = UUID.randomUUID();
-        Name name = Name.create(userRequest.name());
-        Name lastname = Name.create(userRequest.lastname());
-        Email email = Email.create(userRequest.email());
-        String password = userRequest.password();
         UUID roleId = UUID.fromString("318df173-32e1-4865-b380-e3f2343955b4");
 
-        User user = User.create(id, name, lastname, email, password, roleId);
+        User user = User.create(
+                id,
+                userRequest.name(),
+                userRequest.lastname(),
+                userRequest.email(),
+                userRequest.password(),
+                roleId
+        );
+
+        userRepository.save(user);
 
         return new UserResponse(user.getId().getValue());
     }
