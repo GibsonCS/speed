84ea0cbd4296815a4513c40848b13554ea5154e4
GibsonCS <gibson.cruzds@gmail.com>
2026-04-02 18:51:42 -0300

    feat(core): implement role repository in the create user

--- src/main/java/br/com/codelift/speed/core/usecase/CreateUser.java
+++ src/main/java/br/com/codelift/speed/core/usecase/CreateUser.java
@@ -1,6 +1,7 @@
 package br.com.codelift.speed.core.usecase;
 
 import br.com.codelift.speed.core.domain.entity.User;
+import br.com.codelift.speed.core.domain.repository.RoleRepository;
 import br.com.codelift.speed.core.domain.repository.UserRepository;
 import br.com.codelift.speed.exception.BusinessException;
 import br.com.codelift.speed.infrastructure.web.dto.UserRequest;
@@ -10,10 +11,12 @@ import java.util.UUID;
 
 public class CreateUser {
 
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
-    public CreateUser(UserRepository userRepository) {
+    public CreateUser(UserRepository userRepository, RoleRepository roleRepository) {
         this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     public UserResponse execute(UserRequest userRequest) {
@@ -23,7 +26,7 @@ public class CreateUser {
         }
 
         UUID id = UUID.randomUUID();
-        UUID roleId = UUID.fromString("318df173-32e1-4865-b380-e3f2343955b4");
+        UUID roleId = roleRepository.findByName("USER").get().getId();
 
         User user = User.create(
                 id,
