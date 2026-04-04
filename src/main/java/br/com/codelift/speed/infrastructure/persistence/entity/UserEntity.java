7c45883b7ed171611fe1c644589c77111a1f63bc
GibsonCS <gibson.cruzds@gmail.com>
2026-04-04 00:07:07 -0300

    feat(infra): add fetch type to eager

--- src/main/java/br/com/codelift/speed/infrastructure/persistence/entity/UserEntity.java
+++ src/main/java/br/com/codelift/speed/infrastructure/persistence/entity/UserEntity.java
@@ -17,7 +17,7 @@ public class UserEntity {
     private String email;
     private String password;
 
-    @ManyToMany
+    @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(
             name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id"),
