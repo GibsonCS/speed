20caac5ac6cae528e47c25f3810dd7446e34284d
GibsonCS <gibson.cruzds@gmail.com>
2026-04-04 18:50:36 -0300

    feat(infra): add validations in the user request

--- src/main/java/br/com/codelift/speed/infrastructure/web/dto/UserRequest.java
+++ src/main/java/br/com/codelift/speed/infrastructure/web/dto/UserRequest.java
@@ -1,10 +1,15 @@
 package br.com.codelift.speed.infrastructure.web.dto;
 
-public record UserRequest(
+import jakarta.validation.constraints.NotBlank;
 
+public record UserRequest(
+        @NotBlank
         String name,
+        @NotBlank
         String lastname,
+        @NotBlank
         String email,
+        @NotBlank
         String password
 ) {
 }
