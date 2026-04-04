4037c4f7317d0ec72b4f97e3ea5e1cef0da3e56f
GibsonCS <gibson.cruzds@gmail.com>
2026-04-04 18:17:09 -0300

    feat(infra): create user controller

---
+++ src/main/java/br/com/codelift/speed/infrastructure/web/dto/controller/UserController.java
@@ -0,0 +1,44 @@
+package br.com.codelift.speed.infrastructure.web.dto.controller;
+
+
+import br.com.codelift.speed.core.usecase.CreateUser;
+import br.com.codelift.speed.infrastructure.web.dto.UserRequest;
+import br.com.codelift.speed.infrastructure.web.dto.UserResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    private final CreateUser createUser;
+
+    public UserController(CreateUser createUser) {
+        this.createUser = createUser;
+    }
+
+    @PostMapping
+    public ResponseEntity<UserResponse> handleCreateUser(@RequestBody UserRequest userRequest) {
+
+        UserResponse userCreated = createUser.execute(userRequest);
+
+        if (userCreated.id() == null) {
+            ResponseEntity.status(HttpStatus.BAD_REQUEST);
+        }
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(userCreated.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(userCreated);
+    }
+}
