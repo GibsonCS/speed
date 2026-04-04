52e65a71ced8c1a8b56032b6ebc24ccfcd89927a
GibsonCS <gibson.cruzds@gmail.com>
2026-04-04 15:00:46 -0300

    feat(infra): implement to user entity

--- src/main/java/br/com/codelift/speed/infrastructure/persistence/repository/mapper/UserMapper.java
+++ src/main/java/br/com/codelift/speed/infrastructure/persistence/repository/mapper/UserMapper.java
@@ -1,7 +1,9 @@
 package br.com.codelift.speed.infrastructure.persistence.repository.mapper;
 
 import br.com.codelift.speed.core.domain.entity.User;
+import br.com.codelift.speed.infrastructure.persistence.entity.RoleEntity;
 import br.com.codelift.speed.infrastructure.persistence.entity.UserEntity;
+import br.com.codelift.speed.infrastructure.persistence.repository.role.JpaRoleRepository;
 import org.springframework.stereotype.Component;
 
 import java.util.HashSet;
@@ -10,6 +12,12 @@ import java.util.UUID;
 
 @Component
 public class UserMapper {
+
+    private final JpaRoleRepository jpaRoleRepository;
+
+    public UserMapper(JpaRoleRepository jpaRoleRepository) {
+        this.jpaRoleRepository = jpaRoleRepository;
+    }
 
     public User toDomain(UserEntity userEntity) {
 
@@ -24,6 +32,20 @@ public class UserMapper {
                 userEntity.getEmail(),
                 userEntity.getPassword(),
                 roleIds
+        );
+    }
+
+    public UserEntity toEntity(User user) {
+
+        Set<RoleEntity> roles = new HashSet<>(jpaRoleRepository.findAllById(user.getRoleIds()));
+
+        return new UserEntity(
+                user.getId().getValue(),
+                user.getName().getValue(),
+                user.getLastname().getValue(),
+                user.getEmail().getValue(),
+                user.getEncryptedPassword(),
+                roles
         );
     }
 }
