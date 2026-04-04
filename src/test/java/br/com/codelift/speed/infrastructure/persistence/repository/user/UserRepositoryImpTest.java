f263b3e1d68d7ac1b2c9b995b0e7f824e94c9e40
GibsonCS <gibson.cruzds@gmail.com>
2026-04-04 00:11:16 -0300

    test(infra): implement user repository

---
+++ src/test/java/br/com/codelift/speed/infrastructure/persistence/repository/user/UserRepositoryImpTest.java
@@ -0,0 +1,53 @@
+package br.com.codelift.speed.infrastructure.persistence.repository.user;
+
+import br.com.codelift.speed.core.domain.entity.User;
+import br.com.codelift.speed.core.domain.repository.RoleRepository;
+import br.com.codelift.speed.core.domain.repository.UserRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.HashSet;
+import java.util.Optional;
+import java.util.Set;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class UserRepositoryImpTest {
+
+    UUID VALID_ID = UUID.randomUUID();
+    Set<UUID> VALID_ROLE_IDS = new HashSet<>();
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Test
+    void shouldSaveAnUser() {
+        
+        var roleEntity = roleRepository.findByName("USER");
+
+        roleEntity.ifPresent(r -> VALID_ROLE_IDS.add(r.getId()));
+
+        User user = User.create(
+                VALID_ID,
+                "Gibsonz",
+                "silva",
+                "gibson@gibson.com.br",
+                "1234568",
+                VALID_ROLE_IDS
+        );
+
+        userRepository.save(user);
+
+        Optional<User> userSaved = userRepository.findById(VALID_ID);
+
+        userSaved.ifPresent(u -> {
+            assertEquals(VALID_ID, u.getId().getValue());
+        });
+    }
+}
