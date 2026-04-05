aa137fe17766ddf41216c53e9be9599f5f68769b
GibsonCS <gibson.cruzds@gmail.com>
2026-04-05 16:45:27 -0300

    feat(core): prevent get unexists user

--- src/main/java/br/com/codelift/speed/core/usecase/GetUser.java
+++ src/main/java/br/com/codelift/speed/core/usecase/GetUser.java
@@ -2,8 +2,10 @@ package br.com.codelift.speed.core.usecase;
 
 import br.com.codelift.speed.core.domain.entity.User;
 import br.com.codelift.speed.core.domain.repository.UserRepository;
+import br.com.codelift.speed.core.exception.BusinessException;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -18,6 +20,12 @@ public class GetUser {
 
     public User execute(UUID userId) {
 
-        return userRepository.findById(userId).get();
+        Optional<User> userFounded = userRepository.findById(userId);
+
+        if (userFounded.isEmpty()) {
+            throw new BusinessException("User not founded");
+        }
+        
+        return userFounded.get();
     }
 }
