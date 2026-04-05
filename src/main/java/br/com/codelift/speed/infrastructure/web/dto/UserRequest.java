ace55e53245cee5705ae951daadd704ff370ad0b
GibsonCS <gibson.cruzds@gmail.com>
2026-04-04 23:08:47 -0300

    feat(infra): add message in the name field

--- src/main/java/br/com/codelift/speed/infrastructure/web/dto/UserRequest.java
+++ src/main/java/br/com/codelift/speed/infrastructure/web/dto/UserRequest.java
@@ -3,7 +3,7 @@ package br.com.codelift.speed.infrastructure.web.dto;
 import jakarta.validation.constraints.NotBlank;
 
 public record UserRequest(
-        @NotBlank
+        @NotBlank(message = "should not be blank")
         String name,
         @NotBlank
         String lastname,
